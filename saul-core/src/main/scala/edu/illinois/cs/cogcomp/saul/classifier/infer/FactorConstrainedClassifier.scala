aeaaac30b18e2cecf932905178ce6c42e5d511c6
Bhargav Mangipudi <bhargav.mangipudi@gmail.com>
2017-03-23 00:18:04 -0500

    Update. Test unary factor based on original classifier.

--- saul-core/src/main/scala/edu/illinois/cs/cogcomp/saul/classifier/infer/FactorConstrainedClassifier.scala
+++ saul-core/src/main/scala/edu/illinois/cs/cogcomp/saul/classifier/infer/FactorConstrainedClassifier.scala
@@ -6,11 +6,20 @@
   */
 package edu.illinois.cs.cogcomp.saul.classifier.infer
 
-import edu.illinois.cs.cogcomp.lbjava.classify.TestDiscrete
+import cc.factorie.infer.{BP, BPMaxProductRing, BPSummary}
+import cc.factorie.{DenseTensor1, Factor}
+import cc.factorie.model.{DotTemplateWithStatistics1, ItemizedModel, Parameters}
+import cc.factorie.variable._
+import edu.illinois.cs.cogcomp.lbjava.classify.{ScoreSet, TestDiscrete}
+import edu.illinois.cs.cogcomp.lbjava.learn.Softmax
 import edu.illinois.cs.cogcomp.saul.classifier.Learnable
 import edu.illinois.cs.cogcomp.saul.datamodel.edge.Edge
 import edu.illinois.cs.cogcomp.saul.datamodel.node.Node
+import edu.illinois.cs.cogcomp.saul.lbjrelated.LBJLearnerEquivalent
 import edu.illinois.cs.cogcomp.saul.util.Logging
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable
 
 abstract class FactorConstrainedClassifier[T <: AnyRef, HEAD <: AnyRef](val baseNode: Node[T]) extends Logging {
 
@@ -51,7 +60,37 @@ abstract class FactorConstrainedClassifier[T <: AnyRef, HEAD <: AnyRef](val base
   private def testInternal(head: HEAD, performanceReporter: TestDiscrete): Unit = {
     getCandidates(head).foreach({ instance: T =>
       val gold = onClassifier.getLabeler.discreteValue(instance)
-      val prediction = apply(instance)
+      val basePrediction = apply(instance)
+      val softmax = new Softmax()
+
+      val labelSet: ScoreSet = softmax.normalize(onClassifier.classifier.scores(instance))
+      val labels: List[String] = labelSet.values().map(_.asInstanceOf[String]).toList
+
+      // Create a dummy domain for the variable
+      object DummyDomain extends CategoricalDomain[String](labels)
+      class DummyVariable(label: String, val name: String) extends CategoricalVariable[String] {
+        override def domain: CategoricalDomain[String] = DummyDomain
+      }
+
+      val family = new DotTemplateWithStatistics1[DummyVariable] with Parameters {
+        val weights = Weights(new DenseTensor1(DummyDomain.size))
+      }
+
+      DummyDomain.foreach({ domainItem: CategoricalValue[String] =>
+        val score = math.log(labelSet.getScore(domainItem.category).score)
+        family.weights.value(domainItem.intValue) = score
+      })
+
+      val factors = new mutable.ListBuffer[Factor]()
+      val variable = new DummyVariable(basePrediction, "Testing")
+      factors ++= family.factors(variable)
+
+      val model = new ItemizedModel(factors)
+      val fg = BPSummary(Set(variable), BPMaxProductRing, model)
+      BP.inferLoopyMax(fg)
+
+      val maxIndex = fg.marginal(variable).proportions.maxIndex
+      val prediction = DummyDomain(maxIndex).category
 
       performanceReporter.reportPrediction(prediction, gold)
     })
@@ -71,6 +110,7 @@ abstract class FactorConstrainedClassifier[T <: AnyRef, HEAD <: AnyRef](val base
         val gold = onClassifier.getLabeler.discreteValue(instance)
         val prediction = apply(instance)
 
+        logger.info("Unmapped report!")
         tester.reportPrediction(prediction, gold)
       })
     }
