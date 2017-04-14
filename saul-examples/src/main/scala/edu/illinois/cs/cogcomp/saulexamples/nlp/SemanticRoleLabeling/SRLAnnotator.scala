78ca26c6304d0f8d95fd8fa32988a008ac534066
Bhargav Mangipudi <mangipu2@illinois.edu>
2017-04-14 01:56:29 -0500

    Add a unit test for the annotator.

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/SRLAnnotator.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/SRLAnnotator.scala
@@ -6,11 +6,12 @@
   */
 package edu.illinois.cs.cogcomp.saulexamples.nlp.SemanticRoleLabeling
 
-import edu.illinois.cs.cogcomp.annotation.{ Annotator, AnnotatorConfigurator, AnnotatorException }
-import edu.illinois.cs.cogcomp.core.datastructures.{ IntPair, ViewNames }
+import edu.illinois.cs.cogcomp.annotation.{Annotator, AnnotatorConfigurator, AnnotatorException}
+import edu.illinois.cs.cogcomp.core.datastructures.ViewNames
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation._
 import edu.illinois.cs.cogcomp.core.utilities.configuration.ResourceManager
 import edu.illinois.cs.cogcomp.edison.annotators.ClauseViewGenerator
+import edu.illinois.cs.cogcomp.nlp.corpusreaders.AbstractSRLAnnotationReader
 import edu.illinois.cs.cogcomp.saul.classifier.ClassifierUtils
 import edu.illinois.cs.cogcomp.saulexamples.nlp.CommonSensors
 
@@ -21,7 +22,13 @@ class SRLAnnotatorConfigurator extends AnnotatorConfigurator
 class SRLAnnotator(finalViewName: String = ViewNames.SRL_VERB, resourceManager: ResourceManager = new SRLAnnotatorConfigurator().getDefaultConfig)
   extends Annotator(finalViewName, SRLAnnotator.requiredViews, resourceManager) {
   val requiredViewSet: Set[String] = getRequiredViews.toSet
-  lazy val clauseViewGenerator = ClauseViewGenerator.STANFORD
+
+  lazy val clauseViewGenerator: ClauseViewGenerator = {
+    SRLscalaConfigurator.SRL_PARSE_VIEW match {
+      case ViewNames.PARSE_GOLD => new ClauseViewGenerator(ViewNames.PARSE_GOLD, "CLAUSES_GOLD")
+      case ViewNames.PARSE_STANFORD => ClauseViewGenerator.STANFORD
+    }
+  }
 
   override def addView(ta: TextAnnotation): Unit = {
     checkPrequisites(ta)
@@ -31,9 +38,9 @@ class SRLAnnotator(finalViewName: String = ViewNames.SRL_VERB, resourceManager: 
     val finalView = new PredicateArgumentView(getViewName, SRLAnnotator.getClass.getCanonicalName, ta, 1.0)
 
     // Get Predicates in the sentence.
-    val predicates = getPredicates(ta)
+    val allPredicates = getPredicates(ta)
 
-    predicates.foreach({ predicate: Constituent =>
+    allPredicates.foreach({ predicate: Constituent =>
 
       // Get arguments for each predicate detected.
       val argumentList = getArguments(ta, predicate)
@@ -43,6 +50,16 @@ class SRLAnnotator(finalViewName: String = ViewNames.SRL_VERB, resourceManager: 
         argumentList.map(_.getRelationName).toArray,
         argumentList.map(_.getScore).toArray
       )
+
+      // Add additional attributes
+      val lemmaOrToken = ta.getView(ViewNames.LEMMA)
+        .getConstituentsCovering(predicate)
+        .headOption
+        .orElse(ta.getView(ViewNames.TOKENS).getConstituentsCovering(predicate).headOption)
+
+      // TODO - Fix this with correct Sense identifier
+      predicate.addAttribute(AbstractSRLAnnotationReader.SenseIdentifier, "xx")
+      predicate.addAttribute(AbstractSRLAnnotationReader.LemmaIdentifier, lemmaOrToken.map(_.getLabel).getOrElse(""))
     })
 
     assert(finalView.getConstituents.forall(_.getViewName == getViewName), "Verify correct constituent view names.")
@@ -74,7 +91,6 @@ class SRLAnnotator(finalViewName: String = ViewNames.SRL_VERB, resourceManager: 
     }
 
     clauseViewGenerator.addView(ta)
-    assert(ta.hasView(ViewNames.CLAUSES_STANFORD))
   }
 
   /** @param ta Input Text Annotation instance.
@@ -87,10 +103,14 @@ class SRLAnnotator(finalViewName: String = ViewNames.SRL_VERB, resourceManager: 
     SRLMultiGraphDataModel.tokens.populate(CommonSensors.textAnnotationToTokens(ta), train = false, populateEdge = false)
     SRLMultiGraphDataModel.stringTree.populate(Seq(SRLSensors.textAnnotationToStringTree(ta)), train = false, populateEdge = false)
 
-    val predicateCandidates = ta.getView(ViewNames.TOKENS).map(_.cloneForNewView(getViewName))
+    // TODO - Shim to select only Verb for now.
+    val predicateCandidates = ta.getView(ViewNames.POS)
+      .getConstituents
+      .filter(_.getLabel.startsWith("VB"))
+      .map(_.cloneForNewView(getViewName))
     SRLMultiGraphDataModel.predicates.populate(predicateCandidates, train = false, populateEdge = false)
 
-    // Figure out the constants in Boolean Property
+    // TODO - Figure out the constants in Boolean Property
     // TODO - Constant for Predicate label
     predicateCandidates.filter(SRLClassifiers.predicateClassifier(_) == "true").map({ candidate: Constituent =>
       candidate.cloneForNewViewWithDestinationLabel(getViewName, "Predicate")
@@ -128,16 +148,27 @@ class SRLAnnotator(finalViewName: String = ViewNames.SRL_VERB, resourceManager: 
 
     finalRelationList.flatMap { relation: Relation =>
       val label = SRLClassifiers.argumentTypeLearner(relation)
-      if (label == "candidate") None else Some(SRLAnnotator.cloneRelationWithNewLabel(relation, label))
+      if (label == "candidate")
+        None
+      else
+        Some(SRLAnnotator.cloneRelationWithNewLabelAndArgument(relation, label, getViewName))
     }
   }
 }
 
 object SRLAnnotator {
-  private val requiredViews = Array(ViewNames.POS, ViewNames.LEMMA, ViewNames.SHALLOW_PARSE, ViewNames.PARSE_STANFORD)
+  private val requiredViews = Array(
+    ViewNames.POS,
+    ViewNames.LEMMA,
+    ViewNames.SHALLOW_PARSE,
+    SRLscalaConfigurator.SRL_PARSE_VIEW
+  )
 
-  private def cloneRelationWithNewLabel(sourceRelation: Relation, label: String): Relation = {
-    val newRelation = new Relation(label, sourceRelation.getSource, sourceRelation.getTarget, sourceRelation.getScore)
+  private def cloneRelationWithNewLabelAndArgument(sourceRelation: Relation,
+                                                   label: String,
+                                                   targetViewName: String): Relation = {
+    val newTargetConstituent = sourceRelation.getTarget.cloneForNewView(targetViewName)
+    val newRelation = new Relation(label, sourceRelation.getSource, newTargetConstituent, sourceRelation.getScore)
     sourceRelation.getAttributeKeys.foreach({ key: String =>
       newRelation.setAttribute(key, sourceRelation.getAttribute(key))
     })
---
+++ saul-examples/src/test/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/AnnotatorTest.scala
@@ -0,0 +1,38 @@
+/** This software is released under the University of Illinois/Research and Academic Use License. See
+  * the LICENSE file in the root folder for details. Copyright (c) 2016
+  *
+  * Developed by: The Cognitive Computations Group, University of Illinois at Urbana-Champaign
+  * http://cogcomp.cs.illinois.edu/
+  */
+package edu.illinois.cs.cogcomp.saulexamples.nlp.SemanticRoleLabeling
+
+import edu.illinois.cs.cogcomp.core.datastructures.ViewNames
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.{PredicateArgumentView, TextAnnotation}
+import edu.illinois.cs.cogcomp.core.utilities.DummyTextAnnotationGenerator
+import edu.illinois.cs.cogcomp.nlp.corpusreaders.AbstractSRLAnnotationReader
+import org.scalatest.{FlatSpec, Matchers}
+
+class AnnotatorTest extends FlatSpec with Matchers {
+  val textAnnotation: TextAnnotation = DummyTextAnnotationGenerator.generateAnnotatedTextAnnotation(
+    Array(ViewNames.POS, ViewNames.LEMMA, ViewNames.SHALLOW_PARSE, ViewNames.PARSE_GOLD),
+    false,
+    1
+  )
+
+  "SRLAnnotator" should "work" in {
+    val annotator = new SRLAnnotator(ViewNames.SRL_VERB)
+    annotator.addView(textAnnotation)
+
+    assert(textAnnotation.hasView(ViewNames.SRL_VERB), "SRL_VERB view should exist after annotation.")
+
+    val srlView = textAnnotation.getView(ViewNames.SRL_VERB).asInstanceOf[PredicateArgumentView]
+    assert(srlView.getPredicates.size() == 1)
+
+    val verbPredicate = srlView.getPredicates.get(0)
+    assert(srlView.getArguments(verbPredicate).size() >= 1)
+
+    // Required attributes are populated.
+    assert(verbPredicate.hasAttribute(AbstractSRLAnnotationReader.LemmaIdentifier))
+    assert(verbPredicate.hasAttribute(AbstractSRLAnnotationReader.SenseIdentifier))
+  }
+}
