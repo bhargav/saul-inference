7af0ca02cfd95f391c2ee9da70b6da664562f334
Bhargav Mangipudi <mangipu2@illinois.edu>
2017-07-07 22:04:37 -0500

    Check-in changes to evaluation.

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/SRLEvaluation.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/SRLEvaluation.scala
@@ -15,7 +15,7 @@ import edu.illinois.cs.cogcomp.core.datastructures.trees.Tree
 import edu.illinois.cs.cogcomp.core.experiments.ClassificationTester
 import edu.illinois.cs.cogcomp.core.experiments.evaluators.PredicateArgumentEvaluator
 import edu.illinois.cs.cogcomp.core.utilities.protobuf.ProtobufSerializer
-import edu.illinois.cs.cogcomp.curator.CuratorConfigurator.RESPECT_TOKENIZATION
+import edu.illinois.cs.cogcomp.curator.CuratorConfigurator
 import edu.illinois.cs.cogcomp.nlp.utilities.ParseUtils
 import edu.illinois.cs.cogcomp.pipeline.common.PipelineConfigurator.{ USE_LEMMA, USE_POS, USE_SHALLOW_PARSE, USE_STANFORD_PARSE }
 import edu.illinois.cs.cogcomp.saul.util.Logging
@@ -31,119 +31,130 @@ import scala.collection.JavaConverters._
   */
 object SRLEvaluation extends App with Logging {
   val parseViewName = SRLscalaConfigurator.SRL_PARSE_VIEW
-  val predictedViewName = ViewNames.SRL_VERB + "_PREDICTED"
+  val predictedViewName = ViewNames.SRL_VERB + "_PREDICTED_BEAM_10"
+  val srl_model_name = "verb-stanford"
   val annotator = new SRLAnnotator(predictedViewName)
   val sourceDatasetWithPrerequisites = s"Curator=${SRLscalaConfigurator.USE_CURATOR}_${parseViewName}_${TEST_SECTION}.cache"
-  val annotatedDatasetCache = s"${predictedViewName}_${sourceDatasetWithPrerequisites}"
-  private var databaseInstance: Option[DB] = None
+  val annotatedDatasetCache = sourceDatasetWithPrerequisites // s"${srl_model_name}_${sourceDatasetWithPrerequisites}"
+  var databaseInstance: Option[DB] = None
+  val evaluateConstraints = true
 
-  logger.info(s"Initializing the annotator service: USE_CURATOR = ${SRLscalaConfigurator.USE_CURATOR}")
-  val usePipelineCaching = true
-  val annotatorService = SRLscalaConfigurator.USE_CURATOR match {
-    case true =>
-      val nonDefaultProps = new Properties()
-      TextAnnotationFactory.enableSettings(nonDefaultProps, RESPECT_TOKENIZATION)
-      TextAnnotationFactory.createCuratorAnnotatorService(nonDefaultProps)
-    case false =>
-      val nonDefaultProps = new Properties()
-      TextAnnotationFactory.enableSettings(nonDefaultProps, USE_LEMMA, USE_SHALLOW_PARSE)
-      if (!parseViewName.equals(ViewNames.PARSE_GOLD)) {
-        TextAnnotationFactory.enableSettings(nonDefaultProps, USE_POS, USE_STANFORD_PARSE)
+  if (evaluateConstraints == false) {
+    logger.info(s"Initializing the annotator service: USE_CURATOR = ${SRLscalaConfigurator.USE_CURATOR}")
+    val usePipelineCaching = true
+    lazy val annotatorService = SRLscalaConfigurator.USE_CURATOR match {
+      case true =>
+        val nonDefaultProps = new Properties()
+        TextAnnotationFactory.enableSettings(nonDefaultProps, CuratorConfigurator.RESPECT_TOKENIZATION)
+        // TextAnnotationFactory.enableSettings(nonDefaultProps, CuratorConfigurator.CURATOR_FORCE_UPDATE)
+        TextAnnotationFactory.createCuratorAnnotatorService(nonDefaultProps)
+      case false =>
+        val nonDefaultProps = new Properties()
+        TextAnnotationFactory.enableSettings(nonDefaultProps, USE_LEMMA, USE_SHALLOW_PARSE)
+        if (!parseViewName.equals(ViewNames.PARSE_GOLD)) {
+          TextAnnotationFactory.enableSettings(nonDefaultProps, USE_POS, USE_STANFORD_PARSE)
+        }
+        if (!usePipelineCaching) {
+          TextAnnotationFactory.enableSettings(nonDefaultProps, AnnotatorServiceConfigurator.DISABLE_CACHE)
+        }
+        TextAnnotationFactory.createPipelineAnnotatorService(nonDefaultProps)
+    }
+
+    logger.info("Trying to read dataset from cache file.")
+    val cachedDataset = fetchDatasetFromCache(annotatedDatasetCache)
+    val preProcessedDocuments = {
+      if (cachedDataset.nonEmpty) {
+        logger.info("Reading dataset from cache")
+        cachedDataset.map(Some(_))
+      } else {
+        val viewsToKeep = Set(ViewNames.TOKENS, ViewNames.SENTENCE, ViewNames.SRL_VERB, ViewNames.PARSE_GOLD)
+        val testReader = new SRLDataReader(TREEBANK_HOME, PROPBANK_HOME, TEST_SECTION, TEST_SECTION)
+
+        logger.info("Reading the dataset.")
+        testReader.readData()
+
+        logger.info("Annotating documents with pre-requisite views")
+        val dataset = testReader.textAnnotations.asScala
+          .map({ ta =>
+            ta.getAvailableViews
+              .asScala
+              .diff(viewsToKeep)
+              .foreach({ viewName =>
+                logger.debug(s"Removing view $viewName")
+                ta.removeView(viewName)
+              })
+
+            try {
+              // Add new views
+              annotatorService.addView(ta, ViewNames.LEMMA)
+              annotatorService.addView(ta, ViewNames.SHALLOW_PARSE)
+
+              if (!parseViewName.equals(ViewNames.PARSE_GOLD)) {
+                annotatorService.addView(ta, ViewNames.POS)
+                annotatorService.addView(ta, parseViewName)
+              }
+
+              // Clean up the trees
+              val tree: Tree[String] = ta.getView(parseViewName).asInstanceOf[TreeView].getTree(0)
+              val parseView = new TreeView(parseViewName, ta)
+              parseView.setParseTree(0, ParseUtils.stripFunctionTags(ParseUtils.snipNullNodes(tree)))
+              ta.addView(parseViewName, parseView)
+
+              Some(ta)
+            } catch {
+              case ex: Exception =>
+                logger.error(s"Annotation failed for sentence ${ta.getId}; removing it from the list.", ex)
+                None
+            }
+          })
+
+        putDatasetInCache(dataset.flatten, annotatedDatasetCache)
+        dataset
       }
-      if (!usePipelineCaching) {
-        TextAnnotationFactory.enableSettings(nonDefaultProps, AnnotatorServiceConfigurator.DISABLE_CACHE)
-      }
-      TextAnnotationFactory.createPipelineAnnotatorService(nonDefaultProps)
+    }
+
+    val annotatedDocumentsPartial = preProcessedDocuments.partition(_.isEmpty)
+    logger.info("Starting SRL Annotation and evaluation")
+
+    val identifierTester = new ClassificationTester
+    identifierTester.ignoreLabelFromSummary("V")
+
+    val evaluator = new PredicateArgumentEvaluator
+
+    // Annotate with SRL Annotator
+    var srlAnnotationFailures = 0
+    val annotatedDocuments = annotatedDocumentsPartial._2
+      .flatten
+      .flatMap({ ta =>
+        try {
+          annotator.addView(ta)
+          evaluator.evaluate(identifierTester, ta.getView(ViewNames.SRL_VERB), ta.getView(predictedViewName))
+
+          // logger.info(ta.getView(ViewNames.POS).toString)
+          // logger.info(ta.getView(ViewNames.SRL_VERB).toString)
+          // logger.info(ta.getView(predictedViewName).toString)
+
+          Some(ta)
+        } catch {
+          case ex: Exception =>
+            srlAnnotationFailures += 1
+            logger.error(s"SRL Annotation failed for sentence ${ta.getId}.", ex)
+            None
+        }
+      })
+
+    putDatasetInCache(annotatedDocuments, annotatedDatasetCache)
+
+    logger.info(s"Pipeline/Curator Annotation failures = ${annotatedDocumentsPartial._1.size}")
+    logger.info(s"Pipeline/Curator Annotation success = ${annotatedDocumentsPartial._2.size}")
+    logger.info(s"USE_CURATOR = ${SRLscalaConfigurator.USE_CURATOR}")
+    logger.info(s"Documents which failed SRL Annotation = $srlAnnotationFailures")
+    logger.info(identifierTester.getPerformanceTable(true).toTextTable)
+  } else {
+    // Evaluate constraints from available views only
+    val annotatedDataset = fetchDatasetFromCache(annotatedDatasetCache)
   }
 
-  val cachedDataset = fetchDatasetFromCache(sourceDatasetWithPrerequisites)
-  val preProcessedDocuments = {
-    if (cachedDataset.nonEmpty) {
-      cachedDataset
-    } else {
-      val viewsToKeep = Set(ViewNames.TOKENS, ViewNames.SENTENCE, ViewNames.SRL_VERB, ViewNames.PARSE_GOLD)
-      logger.info("Moving existing GOLD annotation views")
-
-      val testReader = new SRLDataReader(TREEBANK_HOME, PROPBANK_HOME, TEST_SECTION, TEST_SECTION)
-
-      logger.info("Reading the dataset.")
-      testReader.readData()
-
-      val dataset = testReader.textAnnotations.asScala
-        .map({ ta =>
-          ta.getAvailableViews
-            .asScala
-            .diff(viewsToKeep)
-            .foreach({ viewName =>
-              logger.debug(s"Removing view $viewName")
-              ta.removeView(viewName)
-            })
-
-          ta
-        })
-
-      putDatasetInCache(dataset, sourceDatasetWithPrerequisites)
-      dataset
-    }
-  }
-
-  logger.info("Annotating documents with pre-requisite views")
-  val annotatedDocumentsPartial = preProcessedDocuments.map({ ta =>
-    try {
-      // Add new views
-      annotatorService.addView(ta, ViewNames.LEMMA)
-      annotatorService.addView(ta, ViewNames.SHALLOW_PARSE)
-
-      if (!parseViewName.equals(ViewNames.PARSE_GOLD)) {
-        annotatorService.addView(ta, ViewNames.POS)
-        annotatorService.addView(ta, parseViewName)
-      }
-
-      // Clean up the trees
-      val tree: Tree[String] = ta.getView(parseViewName).asInstanceOf[TreeView].getTree(0)
-      val parseView = new TreeView(parseViewName, ta)
-      parseView.setParseTree(0, ParseUtils.stripFunctionTags(ParseUtils.snipNullNodes(tree)))
-      ta.addView(parseViewName, parseView)
-
-      Some(ta)
-    } catch {
-      case ex: Exception =>
-        logger.error(s"Annotation failed for sentence ${ta.getId}; removing it from the list.", ex)
-        None
-    }
-  }).partition(_.isEmpty)
-
-  logger.info("Starting SRL Annotation and evaluation")
-
-  val identifierTester = new ClassificationTester
-  identifierTester.ignoreLabelFromSummary("V")
-
-  val evaluator = new PredicateArgumentEvaluator
-
-  // Annotate with SRL Annotator
-  var srlAnnotationFailures = 0
-  val annotatedDocuments = annotatedDocumentsPartial._2
-    .flatten
-    .flatMap({ ta =>
-      try {
-        annotator.addView(ta)
-        evaluator.evaluate(identifierTester, ta.getView(ViewNames.SRL_VERB), ta.getView(predictedViewName))
-        Some(ta)
-      } catch {
-        case ex: Exception =>
-          srlAnnotationFailures += 1
-          logger.error(s"SRL Annotation failed for sentence ${ta.getId}.", ex)
-          None
-      }
-    })
-
-  putDatasetInCache(annotatedDocuments, annotatedDatasetCache)
-
-  logger.info(s"Pipeline/Curator Annotation failures = ${annotatedDocumentsPartial._1.size}")
-  logger.info(s"Pipeline/Curator Annotation success = ${annotatedDocumentsPartial._2.size}")
-  logger.info(s"USE_CURATOR = ${SRLscalaConfigurator.USE_CURATOR}")
-  logger.info(s"Documents which failed SRL Annotation = $srlAnnotationFailures")
-  println(identifierTester.getPerformanceTable(true).toTextTable)
 
   def putDatasetInCache(dataset: Seq[TextAnnotation], datasetName: String): Unit = {
     openDatabase(datasetName)
@@ -172,8 +183,8 @@ object SRLEvaluation extends App with Logging {
       }).map(_.toSeq)
         .getOrElse(Seq.empty)
     } catch {
-      case _: Exception =>
-        logger.error("Error while reading cache file")
+      case ex: Exception =>
+        logger.error("Error while reading cache file", ex)
         Seq.empty
     }
   }
